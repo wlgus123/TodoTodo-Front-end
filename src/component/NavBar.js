a854dfb7c95f0ea14f41255b1b53b7b813229788
wlgus123 <mail30019794@gmail.com>
2024-05-17 01:10:38 +0900

    초기 커밋

---
+++ src/component/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+
+function NavBar() {
+    return (
+        <div>
+            <div>
+                Logo
+            </div>
+            <div>
+                <ul>
+                    <li>Menu</li>
+                    <li>User</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
